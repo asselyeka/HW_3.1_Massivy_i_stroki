8735fe0dece8d76b1064b6dc36d739734a5e61cc
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-09-17 14:37:07 +0600

    Добавьте файлы проекта.

---
+++ HW_3.1_Massivy_i_stroki/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_3._1_Massivy_i_stroki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char enterSymbol=' ';
+                var countSpace = 0;
+            while(enterSymbol!='.')
+            {
+                enterSymbol = Console.ReadKey().KeyChar;
+                if (enterSymbol == ' ') countSpace++;
+            }
+            Console.Write("\nКоличество введенных пробелов: ");
+            Console.WriteLine(countSpace);
+            Console.ReadLine();
+        }
+    }
+}
